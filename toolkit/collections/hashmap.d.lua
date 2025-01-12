1b55b40cc4299c3e1cc6085f092a7d557eab547d
Erik R <erik.riklund@outlook.com>
2025-01-12 22:16:11 +0100

    added hashmap key types

---
+++ toolkit/collections/hashmap.d.lua
@@ -0,0 +1,19 @@
+---@meta
+
+--[[~ Updated: 2025/01/12 | Author(s): Gopher ]]
+--
+-- Backbone - An addon development framework for World of Warcraft.
+--
+--This program is free software: you can redistribute it and/or modify it under the terms
+--of the GNU General Public License as published by the Free Software Foundation, either
+--version 3 of the License, or (at your option) any later version.
+--
+--This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+--without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+--See the GNU General Public License <https://www.gnu.org/licenses/> for more details.
+
+---
+---
+---
+---@alias backbone.hashmap-key-type string|number|boolean|table|function|userdata|thread
+---
